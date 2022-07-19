60ccc4271d272af0cf3e3dc0a10ba773cd946486
IstiakAhmed-1721365 <s.m.ahmed@northsouth.edu>
2022-07-19 17:16:46 +0900

    created loading screen

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h2>Post list</h2>
+  <div>
+    <div v-if="loading">
+      <h2> Loading </h2>
+    </div>
+    <div v-else>
+      <ul class="list-group list-group-flush">
+        <li v-for="post in posts" :key="post.id" class="list-group-item">
+          {{ post.title}}
+          <span>
+            {{ post.body}}
+          </span>
+      </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "PostList",
+  data() { 
+    return {
+      loading: true,
+      posts: [],
+    };
+},
+async mounted() {
+  var results = await axios.get("http://jsonplaceholder.typicode.com/posts");
+  this.loading = false;
+  this.posts = results.data;
+  console.log(results);
+  
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
